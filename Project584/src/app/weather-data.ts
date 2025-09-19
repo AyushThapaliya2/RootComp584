5e7a219f2e4a60ca4d4636e926207d7f7bc22434
Thapaliya <ayush.thapaliya.36@my.csun.edu>
2025-09-19 16:46:07 -0700

    Updated-1

--- Project584/src/app/weather-data.ts
+++ Project584/src/app/weather-data.ts
@@ -1,2 +1,6 @@
 export interface WeatherData {
+    Date: string,
+    TemperatureC: number,
+    TemperatureF: number,
+    Summary: string
 }
--- Project584/src/app/weather/weather.ts
+++ Project584/src/app/weather/weather.ts
@@ -1,11 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { WeatherData } from '../weather-data';
 
 @Component({
   selector: 'app-weather',
   imports: [],
   templateUrl: './weather.html',
-  styleUrl: './weather.css'
+  styleUrl: './weather.scss'
 })
 export class Weather {
-
+  constructor(http: HttpClient) {
+    http.get<WeatherData[]>('http://localhost:5262/weatherforecast').subscribe(result => {
+    
+    });
+  }
 }
