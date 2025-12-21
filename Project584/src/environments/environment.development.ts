42b2a22e4413791ec05c6a4f3df1b345fe535bfc
Ayush <thapaliya.ayush1999@gmail.com>
2025-12-20 22:42:46 -0800

    Frontend final update

--- Project584/src/environments/environment.development.ts
+++ Project584/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    apiUrl: 'http://localhost:5145/'
+  production: false,
+  apiUrl: 'http://44.222.108.63/',
 };
--- Project584/src/environments/environment.ts
+++ Project584/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '',
+  apiUrl: 'http://44.222.108.63/',
 };
